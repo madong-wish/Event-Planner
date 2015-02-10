d1f2c6e3b78818b321f8cfd2a0d5f893eb3477c6
manduo-dong <manduo.dong@auto-grid.com>
2015-02-10 12:17:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventScheduler::Application.config.secret_token = '7bafd7133f6e1c05c955d5b2a002600d51d1b7b6b450461a875edd6543341724cce2ff54f1740bb56c07b3ed1b7bfda6e55494304841095887e47a543e0ba312'
