20d7bf183a7b78d49452262eb64ff259515c2072
manduo-dong <manduo.dong@auto-grid.com>
2015-04-05 22:25:10 -0700

    production env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 EventScheduler::Application.initialize!
+
+RAILS_ENV = 'development'
+RACK_ENV = 'development'
