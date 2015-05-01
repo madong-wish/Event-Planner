cc8ef4871e99772ce03f36c472f70fa83973a777
manduo-dong <manduo.dong@auto-grid.com>
2015-04-30 22:24:07 -0700

    Sample event

--- app/assets/javascripts/angular/controllers/overview.js.coffee
+++ app/assets/javascripts/angular/controllers/overview.js.coffee
@@ -4,6 +4,8 @@ app = angular.module("Event", ["ngResource"])
 app.controller(@EventCtrl = ($scope, $document) ->
   $scope.example_events = ["Crab fishing in Sausalito", "Hike in Dry Creek, Hayward", "Walk in Lands End San Francisco", "Visit Tech Museum in San Jose", "Watch movie in Union Landing"]
   $scope.add_event_flag = false
+  $scope.rsvp_num = 0
+  $scope.rsvped = false
   console.log($document[0].cookie)
   unless angular.isUndefined($document[0].cookie)
     $scope.username = $document[0].cookie.split('username=')[1].split(';')[0]
@@ -18,4 +20,17 @@ app.controller(@EventCtrl = ($scope, $document) ->
   $scope.generateSampleEvent = () ->
     unless $scope.add_event_flag
       $scope.example_events[Math.floor(Math.random()*5)]
+
+  $scope.createEvent = () ->
+    window.alert('You do not have privilege to create event.')
+
+  $scope.rsvp = () ->
+    if $scope.rsvped
+      $scope.rsvp_num = 0
+      $scope.rsvped = false
+      $browser.sleep(500)
+    else
+      $scope.rsvp_num = 1
+      $scope.rsvped = true
+      $browser.sleep(500)
 )
--- public/assets/application.js
+++ public/assets/application.js
@@ -25715,6 +25715,8 @@ return jQuery;
   app.controller(this.EventCtrl = function($scope, $document) {
     $scope.example_events = ["Crab fishing in Sausalito", "Hike in Dry Creek, Hayward", "Walk in Lands End San Francisco", "Visit Tech Museum in San Jose", "Watch movie in Union Landing"];
     $scope.add_event_flag = false;
+    $scope.rsvp_num = 0;
+    $scope.rsvped = false;
     console.log($document[0].cookie);
     if (!angular.isUndefined($document[0].cookie)) {
       $scope.username = $document[0].cookie.split('username=')[1].split(';')[0];
@@ -25726,9 +25728,23 @@ return jQuery;
     $scope.cancelEvent = function() {
       return $scope.add_event_flag = false;
     };
-    return $scope.generateSampleEvent = function() {
+    $scope.generateSampleEvent = function() {
       if (!$scope.add_event_flag) {
         return $scope.example_events[Math.floor(Math.random() * 5)];
+      }
+    };
+    $scope.createEvent = function() {
+      return window.alert('You do not have privilege to create event.');
+    };
+    return $scope.rsvp = function() {
+      if ($scope.rsvped) {
+        $scope.rsvp_num = 0;
+        $scope.rsvped = false;
+        return $browser.sleep(500);
+      } else {
+        $scope.rsvp_num = 1;
+        $scope.rsvped = true;
+        return $browser.sleep(500);
       }
     };
   });
